cc975e501039c8e7f901ac2651c5da06d808c938
AmalNadifi <amal.nadifi9@gmail.com>
2024-02-15 12:24:17 +0000

    Task0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""The following module is for a single function that adds two integers."""
+
+
+def add(x: float, y: float) -> float:
+    """Adds two floats and returns their sum."""
+    return x + y
