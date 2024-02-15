b2beba6c89925b54e97cbc9bf64467b2396f7ede
AmalNadifi <amal.nadifi9@gmail.com>
2024-02-15 12:42:59 +0000

    Task3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines a function to convert a float to a string representation"""
+
+def to_str(n: float) -> str:
+    """Converts a float to its string representation.
+    
+    Args:
+        n: A float to be converted to a string.
+        
+    Returns:
+        A string representation of the float.
+    """
+    return str(n)
